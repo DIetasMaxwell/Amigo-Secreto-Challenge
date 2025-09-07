d219be798ab48fadfd8e2774d73d41ba4ab12a88
DietasMaxwell <socdie231564@gmail.com>
2025-09-07 17:48:08 -0500

    indicar usar un solo nombre y crear la función de lista de amigos

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//Arrary para introducir los nombres de los amigos.
+let listaAmigos = [];
 
 
-let insertarAmigos = [];
+
+
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del Número Secreto.'); 
+    asignarTextoElemento('p', `Selecciona un número del 1 al ${numeroMaximo}.`); 
+    numeroSecreto = generarNumeroSecretro();
+    intentos = 1;
+}
+
+condicionesIniciales();
