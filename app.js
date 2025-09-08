5aa4c0c7064e8ad3fe4f489ccc27b5bbfc6236a4
DietasMaxwell <socdie231564@gmail.com>
2025-09-07 19:32:32 -0500

    La lista ya guarda nombres, falta agregar las demás condicionales sobre solo aceptar letras y no aceptar espacios en blanco

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-//Arrary para introducir los nombres de los amigos.
+//Arrary para introducir los nombres de los amigos (inicia vacío).
 let listaAmigos = [];
 
 
+//Esta función busca en el html el nombre del 
+function agregarAmigo () {
+    let nombre = document.getElementById("nombreDelAmigo").value;
+    // Mirar en internet sobre cómo poner las condiciones de que no se introduzcan espacios y solo se ingrese un nombre (letras y sin espacios). 
+    //Push antes de acutalizar para evitar retrasos
+    listaAmigos.push(nombre);
+    actualizarListaDeAmigos();
+    document.getElementById("nombreDelAmigo").value = "";
 
-
-
-function condicionesIniciales() {
-    asignarTextoElemento('h1', 'Juego del Número Secreto.'); 
-    asignarTextoElemento('p', `Selecciona un número del 1 al ${numeroMaximo}.`); 
-    numeroSecreto = generarNumeroSecretro();
-    intentos = 1;
 }
 
-condicionesIniciales();
+//Si el nombre cumple con las condiciones, debe iniciarse esta función.
+function actualizarListaDeAmigos() { 
+    //Busca el elemento que tenga el atributo del id en el html y lo guarda lo que se introduce en la variable lista
+    let lista = document.getElementById ("listaAmigos");
+    //Borra y vuelve a poner los valores para evitar repetir los nombres (acepta los nombres duplicados, pero no menciona más de una vez cada uno de los valores de la lista) cada vez que se usa la función
+    lista.innerHTML = ""; 
+    //recorre el array y mdice cuantos elementos tiene
+    for (let i=0 ; i<listaAmigos.length; i++) { 
+        //para el "ul" de html se utiliza el "li"
+        //crea un li nuevo y vacío en el array
+        let li = document.createElement("li");
+        //cambia el contenido dl nuevo li para ponerle el nombre del amigo.
+        li.textContent = listaAmigos[i]; 
+        //ingresa el li en el id para constuir la lista y su bucle
+        lista.appendChild (li);
+        console.log(listaAmigos);
+    }
+}
+
+
+
+
